59a038757cc324a24dbbebe98fbc61e8c7fa3e7a
voronaalina <voronaalina991@gmail.com>
2024-11-11 12:03:37 +0300

    main commit

--- webphotos.py
+++ webphotos.py
@@ -9,10 +9,13 @@ os.makedirs("images", exist_ok=True)
 images = soup.find_all('img')
 for i, img in enumerate(images):
     img_url = img.get('src')
-    if not img_url.startswith("http"):
-        img_url = url + img_url
-        img_data = requests.get(img_url).content
-    with open("images/image_" + str(i) + ".jpg", 'wb') as img_file:
-        img_file.write(img_data)
-    print("збережено" + "images/image_" +str(i) + ".jpg")
-    
+    if img_url:
+        if not img_url.startswith("http"):
+            img_url = 'https://www.bbc.com' + img_url  
+        try:
+            img_data = requests.get(img_url).content
+            with open("c:\\lab14\\" + "images/image_" + str(i) +".jpg", 'wb') as img_file:
+                img_file.write(img_data)
+            print("збережено " + "images/image_" + str(i) + ".jpg")
+        except requests.RequestException:
+            print("Помилка")
